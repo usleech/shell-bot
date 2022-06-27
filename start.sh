b73003f7c8169e3d73746ce7c2db90bebe7f2028
usleech <71797515+usleech@users.noreply.github.com>
2022-06-27 18:19:46 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,22 @@
-if [[ -z "$UPSTREAM_BRANCH" ]] ; then
+if [[ -n $UPSTREAM_BRANCH ]]; then
+    BRAN = $UPSTREAM_BRANCH
+else
     BRAN = 'master'
-else
-    BRAN = "$UPSTREAM_BRANCH"
 fi
-if [[ -z "$UPSTREAM_REPO" ]] ; then
-    echo "Please set UPSTREAM_REPO"
-    exit 0
-else
+
+if [[ -n "$UPSTREAM_REPO" ]] ; then
     [ -d ".git" ] && rm -rf .git*
     git init -q
     git config --global user.email usleech@gmail.com
     git config --global user.name usleech
     git add .
     git commit -sm update -q
-    git remote add origin "$UPSTREAM_REPO"
+    git remote add origin $UPSTREAM_REPO
     git fetch origin -q
     git reset --hard origin/$BRAN -q
+else
+    echo "Please set UPSTREAM_REPO"
+    exit 0
 fi
 
 bash run
